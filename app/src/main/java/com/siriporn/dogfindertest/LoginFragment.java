247a75c5e8b7558cd20762921bab9c0519ed8256
oilsrp23346 <oilsrp23346@gmail.com>
2017-01-13 15:31:23 +0700

    get FB Data
    "id, first_name, last_name, email,gender, birthday, location"
    "profile_pic"

--- app/src/main/java/com/siriporn/dogfindertest/LoginFragment.java
+++ app/src/main/java/com/siriporn/dogfindertest/LoginFragment.java
@@ -1,5 +1,6 @@
 package com.siriporn.dogfindertest;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -16,13 +17,22 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.Profile;
 import com.facebook.ProfileTracker;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import static android.R.attr.data;
 import static android.R.attr.tag;
+import static com.facebook.login.widget.ProfilePictureView.TAG;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -37,11 +47,29 @@ public class LoginFragment extends Fragment {
         @Override
         public void onSuccess(LoginResult loginResult) {
             Log.d("VIVZ", "onSuccess");
-            AccessToken accessToken = loginResult.getAccessToken();
             Profile profile = Profile.getCurrentProfile();
             mTextDetails.setText(constructWelcomeMessage(profile));
-        }
 
+
+            //get data from fb
+            String accessToken = loginResult.getAccessToken().getToken();
+            Log.i("accessToken", accessToken);
+
+            GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+
+                @Override
+                public void onCompleted(JSONObject object, GraphResponse response) {
+                    Log.i("LoginActivity", response.toString());
+                    // Get facebook data from login
+                    Bundle bFacebookData = getFacebookData(object);
+                }
+            });
+            Bundle parameters = new Bundle();
+            parameters.putString("fields", "id, first_name, last_name, email,gender, birthday, location");
+            request.setParameters(parameters);
+            request.executeAsync();
+        }
+        //end
         @Override
         public void onCancel() {
             Log.d("VIVZ", "onCancel");
@@ -52,6 +80,44 @@ public class LoginFragment extends Fragment {
             Log.d("VIVZ", "onError " + e);
         }
     };
+
+    private Bundle getFacebookData(JSONObject object) {
+
+        try {
+            Bundle bundle = new Bundle();
+            String id = object.getString("id");
+
+            try {
+                URL profile_pic = new URL("https://graph.facebook.com/" + id + "/picture?width=200&height=150");
+                Log.i("profile_pic", profile_pic + "");
+                bundle.putString("profile_pic", profile_pic.toString());
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                return null;
+            }
+
+            bundle.putString("idFacebook", id);
+            if (object.has("first_name"))
+                bundle.putString("first_name", object.getString("first_name"));
+            if (object.has("last_name"))
+                bundle.putString("last_name", object.getString("last_name"));
+            if (object.has("email"))
+                bundle.putString("email", object.getString("email"));
+            if (object.has("gender"))
+                bundle.putString("gender", object.getString("gender"));
+            if (object.has("birthday"))
+                bundle.putString("birthday", object.getString("birthday"));
+            if (object.has("location"))
+                bundle.putString("location", object.getJSONObject("location").getString("name"));
+
+            return bundle;
+
+        } catch (JSONException e) {
+            Log.d(TAG, "Error parsing JSON");
+            return null;
+        }
+    }
 
     public LoginFragment() {
     }
@@ -128,9 +194,11 @@ public class LoginFragment extends Fragment {
     private String constructWelcomeMessage(Profile profile) {
         StringBuffer stringBuffer = new StringBuffer();
         if (profile != null) {
-            stringBuffer.append("Welcome " + profile.getName() + profile.getProfilePictureUri(20,20));
+             stringBuffer.append(profile.getName());
+
         }
         return stringBuffer.toString();
     }
+
 
 }
